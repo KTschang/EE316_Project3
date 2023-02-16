f018a9bd54783b98aa9b0d1f445d209dc3168d6f
Keith Tschang <tschankl@clarkson.edu>
2023-02-16 12:27:31 -0500

    Added first i2c files
    Signed-off-by: Keith Tschang <tschankl@clarkson.edu>

---
+++ EE316_Project3.srcs/sources_1/new/i2c_lcd_userlogic.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/16/2023 12:24:43 PM
+-- Design Name: 
+-- Module Name: i2c_lcd_userlogic - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity i2c_lcd_userlogic is
+    Port ( clk : in STD_LOGIC);
+end i2c_lcd_userlogic;
+
+architecture Behavioral of i2c_lcd_userlogic is
+
+begin
+
+
+end Behavioral;
